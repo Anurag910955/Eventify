b9d6b65c890e7b011d644ab7a33efd325f055f03
Anurag910955 <145783931+Anurag910955@users.noreply.github.com>
2025-08-12 21:22:43 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Health check route
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
+  res.redirect('/');
   res.send('API is running...');
 });
 // Contact route
@@ -50,18 +51,6 @@ app.use('/api/verify-email', otpRoutes);
 // Error handling middleware
 app.use(errorHandler);
 
-// Get current directory path
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Serve static files from the React build folder
-app.use(express.static(path.join(__dirname, 'frontend', 'dist'))); // for Vite
-// app.use(express.static(path.join(__dirname, 'frontend', 'build'))); // for CRA
-
-// Always return index.html for React Router routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-});
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
